d67960b94b1f4f145cbbf29ecb6d7ed58fc0b73f
Kris57 <49682577+yuuki76@users.noreply.github.com>
2021-06-11 22:06:12 +0900

    Init

--- autofft.sh
+++ autofft.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-convert $1".png" -gravity North -background black -extent 1920x1920 $1"ext.png"
-convert $1"ext.png" -fft -delete 1 -contrast-stretch 0 -evaluate log 100000 -gamma 2 $1"eval5g2ct0fft.png"
-convert $1"ext.png" -fft -delete 1 -evaluate log 100000 $1"evalonly.png"
-convert $1"ext.png" -fft $1"fft-debug.png"
+FILENAME=${1%.*}
+convert $FILENAME".png" -gravity North -background black -extent 1920x1920 -fft +depth $FILENAME"temp.miff"
+convert $FILENAME"temp.miff[0]" -contrast-stretch 0 -evaluate log 1000000 $FILENAME"L6-FFT-c.png"
+convert $FILENAME"temp.miff[0]" -evaluate log 1000000 $FILENAME"rawfft.png"
+#convert $FILENAME"ext.png" -fft $FILENAME"fft-debug.png"
