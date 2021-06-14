328a69cf674b602e7c481aedddd45813d650376b
Kris57 <49682577+yuuki76@users.noreply.github.com>
2021-06-14 09:21:13 +0900

    Init

--- autofft.sh
+++ autofft.sh
@@ -2,5 +2,9 @@
 FILENAME=${1%.*}
 convert $FILENAME".png" -gravity North -background black -extent 1920x1920 -fft +depth $FILENAME"temp.miff"
 convert $FILENAME"temp.miff[0]" -contrast-stretch 0 -evaluate log 1000000 $FILENAME"L6-FFT-c.png"
+#convert $FILENAME"temp.miff[0]" -auto-level -evaluate log 1000000 $FILENAME"L6-FFT-a.png"
+#convert $FILENAME"temp.miff[0]" -normalize -evaluate log 1000000 $FILENAME"L6-FFT-n.png"
 convert $FILENAME"temp.miff[0]" -evaluate log 1000000 $FILENAME"rawfft.png"
+#convert $FILENAME"temp.miff[0]" -evaluate log 1000000 -normalize $FILENAME"rawfft-n.png"
+#convert $FILENAME"temp.miff[0]" -evaluate log 1000000 -contrast-stretch 0 $FILENAME"rawfft-c.png"
 #convert $FILENAME"ext.png" -fft $FILENAME"fft-debug.png"
